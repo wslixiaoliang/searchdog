bd436f129efe4102455afe73a343fbce1bae7b7f
wslixiaoliang <liliang11@asiainfo.com>
2019-04-27 20:34:40 +0800

    完成url地址访问

---
+++ photographycore/photo-util/src/main/java/com/art/util/Constans.java
@@ -0,0 +1,24 @@
+package com.art.util;
+
+/**
+ * 常量类
+ */
+public class Constans {
+
+    /**
+     * 分页常量
+     */
+    public static final String START = "start";
+    public static final String LIMIT = "limit";
+
+    /**
+     * 返回编码
+     */
+    public static final String SUCESSS_RETURN_CODE = "0";
+    public static final String FAILURE_RETURN_CODE = "-9999";
+
+
+
+
+
+}
