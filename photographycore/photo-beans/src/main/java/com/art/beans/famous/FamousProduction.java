df9577ecf229257391b4e0149253688ce2ffc41e
liliang <liliang11@asiainfo.com>
2019-05-20 21:28:55 +0800

    FAMOUS-作品展览馆样式调试新增摘要字段-20190520

--- photographycore/photo-beans/src/main/java/com/art/beans/famous/FamousProduction.java
+++ photographycore/photo-beans/src/main/java/com/art/beans/famous/FamousProduction.java
@@ -9,10 +9,12 @@ public class FamousProduction implements Serializable {
 
     private long productionId;//作品ID
     private long famousId;//名人ID
+    private String portraitName;//肖像名
     private String chineseName;//中文名
     private String englishName;//英文名
     private String productionName;//作品名称
     private String publishedYear;//发表年份
+    private String summaryInfo;//作品摘要
     private String productionContent;//作品内容
 
     //无参构造方法
@@ -20,14 +22,16 @@ public class FamousProduction implements Serializable {
     }
 
     //有参构造方法
-    public FamousProduction(long productionId, long famousId, String productionName, String publishedYear, String productionContent, String chineseName, String englishName) {
+    public FamousProduction(long productionId, long famousId, String portraitName, String chineseName, String englishName, String productionName, String publishedYear, String summaryInfo, String productionContent) {
         this.productionId = productionId;
         this.famousId = famousId;
-        this.productionName = productionName;
-        this.publishedYear = publishedYear;
-        this.productionContent = productionContent;
+        this.portraitName = portraitName;
         this.chineseName = chineseName;
         this.englishName = englishName;
+        this.productionName = productionName;
+        this.publishedYear = publishedYear;
+        this.summaryInfo = summaryInfo;
+        this.productionContent = productionContent;
     }
 
     //getter and setter
@@ -85,5 +89,21 @@ public class FamousProduction implements Serializable {
 
     public void setEnglishName(String englishName) {
         this.englishName = englishName;
+    }
+
+    public String getPortraitName() {
+        return portraitName;
+    }
+
+    public void setPortraitName(String portraitName) {
+        this.portraitName = portraitName;
+    }
+
+    public String getSummaryInfo() {
+        return summaryInfo;
+    }
+
+    public void setSummaryInfo(String summaryInfo) {
+        this.summaryInfo = summaryInfo;
     }
 }
