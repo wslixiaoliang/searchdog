3fa3845752277e15dab1f1c5275969c17493c755
liliang <liliang11@asiainfo.com>
2019-05-20 11:46:50 +0800

    FAMOUS-作品页面展示多个作品调试-20190520

--- photographycore/photo-web/src/main/resources/public/js/production.js
+++ photographycore/photo-web/src/main/resources/public/js/production.js
@@ -35,17 +35,56 @@ $(document).ready(function () {
          success:function(result){
              if(null!=result && result.length > 0){
 
-                 var pName = result[0].productionName;
-                 var pYear = result[0].publishedYear;
-                 var fChineseName = result[0].chineseName;
-                 var fEnglishName = result[0].englishName;
-                 var pContent = result[0].productionContent;
+                 for(var i=0;i<result.length;i++){
 
-                 $(".pName").html(pName);
-                 $(".fChineseName").html(fChineseName);
-                 $(".fEnglishName").html(fEnglishName);
-                 $(".pYear").html(pYear);
-                 $(".pContent").html(pContent);
+                     var u = "titleDiv" +i;
+                     var v = "pName" + i;
+                     var w = "fChineseName" + i;
+                     var x = "fEnglishName" + i;
+                     var y = "pYear"+i;
+                     var z = "pContent" + i ;
+
+                     //总div
+                     var proDiv = document.getElementById("proDiv")
+                     //标题div
+                     var titleDiv = document.createElement("div");
+                     titleDiv.setAttribute("class",u);
+                     titleDiv.setAttribute("style","width: 300px;height: 150px; margin: auto;margin-top:40px;text-align: center;")
+                     proDiv.appendChild(titleDiv); //标题div加入总div
+                     //作品名
+                     var pName = document.createElement("p");
+                     pName.setAttribute("class",v);
+                     pName.setAttribute("style","font-family: \"黑体\";font-size: 16px;");
+                     pName.innerText = result[i].productionName;
+                     titleDiv.appendChild(pName);
+                     //作者中文名
+                    var fChineseName = document.createElement("p");
+                    fChineseName.setAttribute("class",w);
+                    fChineseName.setAttribute("style","font-family: \"微软雅黑\";font-size: 14px;");
+                    fChineseName.innerText = result[i].chineseName;
+                    titleDiv.appendChild(fChineseName);
+                    //作者英文名
+                    var fEnglishName = document.createElement("p");
+                    fEnglishName.setAttribute("class",x);
+                    fEnglishName.setAttribute("style","font-family: \"微软雅黑\";font-size: 14px;");
+                    fEnglishName.innerText = result[i].englishName;
+                    titleDiv.appendChild(fEnglishName);
+                    //发表年份
+                    var pYear = document.createElement("p");
+                    pYear.setAttribute("class",y);
+                    pYear.setAttribute("style","font-family:\"楷体\";font-style: oblique;font-size: 14px;");
+                    pYear.innerText = result[i].publishedYear;
+                    titleDiv.appendChild(pYear);
+
+                    var pContent = document.createElement("div");
+                     pContent.setAttribute("class",z);
+                     pContent.setAttribute("style","width: 800px;height: auto;margin: auto;font-family: \"微软雅黑\";font-size:14px;");
+                     pContent.innerText = result[i].productionContent;
+                     proDiv.appendChild(pContent);//内容div加入总div
+
+
+                 }
+
              }
          }
 
