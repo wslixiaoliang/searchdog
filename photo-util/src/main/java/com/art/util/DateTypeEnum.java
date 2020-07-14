a1117ffed7f5b0160fcb4c5d94253e4edc5524e1
wslixiaoliang <liliang11@asiainfo.com>
2020-07-14 22:14:10 +0800

    #20200714 新增时间枚举类

---
+++ photo-util/src/main/java/com/art/util/DateTypeEnum.java
@@ -0,0 +1,36 @@
+package com.art.util;
+
+/**
+ * 时间格式 枚举
+ * @author wslixiaoliang
+ */
+public enum DateTypeEnum {
+
+    YYY_MM("yyyy-MM","yyyy-MM"),
+    YYYY_MM_DD("yyyy-MM-dd","yyyy-MM-dd"),
+    YYYY_MM_DD_HH_MM_SS("yyyy-MM-dd HH:mm:ss","yyyy-MM-dd HH:mm:ss");
+
+    private String code;
+    private String value;
+
+    DateTypeEnum(String code, String value) {
+        this.code = code;
+        this.value = value;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
--- photo-web/src/test/java/com/art/web/elastic/ElasticTest.java
+++ photo-web/src/test/java/com/art/web/elastic/ElasticTest.java
@@ -1,6 +1,8 @@
 package com.art.web.elastic;
 
 import com.art.beans.elastic.SearchResult;
+import com.art.util.DateTypeEnum;
+import com.art.util.DateUtil;
 import com.art.web.WebApplication;
 import com.art.web.component.elastic.IndexComponent;
 import com.art.web.component.elastic.SearchComponent;
@@ -10,12 +12,16 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * 搜索引擎底层方法测试类
+ *
  * @author wslixiaoliang
  */
 @RunWith(SpringRunner.class)
@@ -27,11 +33,10 @@ public class ElasticTest {
     private static final Logger LOGGER = Logger.getLogger(ElasticTest.class);
 
     @Test
-    public void testSearch() throws IOException
-    {
+    public void testSearch() throws IOException {
         Map<String, Object> document = new HashMap<>();
 //        document.put("keywordId","100");
-        document.put("chineseName","阿尔伯特");
+        document.put("chineseName", "阿尔伯特");
 //        document.put("landingTime","2999-10-11");
 
 //        SearchResult createResult = searchDocumentsComponent.createOrUpdating("photo.search_keyword","search_keyword","100",document);//新增/更新
@@ -46,34 +51,46 @@ public class ElasticTest {
 
 //        System.out.println("新增结果：======================"+createResult);
 //        System.out.println("主键查询结果：======================"+searchIdResult);
-        System.out.println("字段查询结果：======================"+searchResult.getDocuments());
+        System.out.println("字段查询结果：======================" + searchResult.getDocuments());
 //        System.out.println("删除结果：======================"+deleteResult);
 
 //        LOGGER.info("新增结果：======================"+createResult);
 //        LOGGER.info("主键查询结果：======================"+searchIdResult);
-        LOGGER.info("字段查询结果：======================"+searchResult);
+        LOGGER.info("字段查询结果：======================" + searchResult);
 //        LOGGER.info("删除结果：======================"+deleteResult);
 
     }
 
     @Test
-    public void getRoudam(){
-            long pnc = 0;
-            for(int i=1;i<=3;i++){
-                for(int j=1;j<=3;j++){
-                    for(int k=1;k<=3;k++){
-                        if(i!=j && i!=k && j!=k){
-                            pnc++;
-                            StringBuffer num = new StringBuffer();
-                            num.append(i);
-                            num.append(j);
-                            num.append(k);
-                            num.append(" ");
-                            System.out.print(num);
-                        }
+    public void getRoudam() {
+        long pnc = 0;
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                for (int k = 1; k <= 3; k++) {
+                    if (i != j && i != k && j != k) {
+                        pnc++;
+                        StringBuffer num = new StringBuffer();
+                        num.append(i);
+                        num.append(j);
+                        num.append(k);
+                        num.append(" ");
+                        System.out.print(num);
                     }
                 }
             }
-            System.out.println("\n" + "随机组合数的个数为： "+ pnc );
+        }
+        System.out.println("\n" + "随机组合数的个数为： " + pnc);
+    }
+
+    /**
+     * 时间格式化-long型 转化成 String类型 （切记long型后面要加L）
+     */
+    @Test
+    public void testLong2StringTime() {
+        long time  = System.currentTimeMillis();
+        Date date = new Date(time);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String nowTime = sdf.format(date);
+        System.out.println("==============现在的时间为："+nowTime);
     }
 }
